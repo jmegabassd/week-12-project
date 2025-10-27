fe8b837df52977f4719db37a0da6754b0199fb3d
Pigeonlover <bravefoot@outlook.com>
2025-10-27 11:55:22 +0000

    feature: set up clerk authentication, authorisation, components (header, footer, sidebar)

---
+++ src/app/about/page.js
@@ -0,0 +1,5 @@
+// About page
+
+export default function AboutPage() {
+  return <div>About our site.</div>;
+}
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,37 @@
+//
+
+import {
+  UserButton,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+} from "@clerk/nextjs";
+
+//this auth property contains the user's data object (you can check this in you clerk application > Users > Click on a user > Show JSON)
+import { auth } from "@clerk/nextjs/server";
+
+export default async function SidebarPage() {
+  //auth is async
+  // we can use it to destructure the userId that clerk creates on sign-up
+  const { userId } = await auth();
+
+  return (
+    <>
+      {/* we are conditionally rendering these buttons depending on the user being authenticated or not */}
+      <SignedIn>
+        <UserButton />
+        {/* this is sensitive data, just for the demo */}
+        <p>{userId}</p>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton>
+          <button>Sign in here</button>
+        </SignInButton>
+        <SignUpButton>
+          <button>Sign up here</button>
+        </SignUpButton>
+      </SignedOut>
+    </>
+  );
+}
