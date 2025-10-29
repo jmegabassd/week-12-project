8c315310cd0ce1bb3a20f94d563e741a030bd9eb
DARE VICTOR OTITOJU <otitojudare@gmail.com>
2025-10-29 11:48:56 +0000

    Chore: Fix metadata for static pages

--- src/app/adventures/page.js
+++ src/app/adventures/page.js
@@ -1,6 +1,11 @@
 import Link from "next/link";
 import CharacterComponent from "@/components/Character";
 
+export const metadata = {
+  title: "Adventure Page - No-Name RPG",
+  description: "RPG - Text Adventure Game",
+};
+
 export default function AdventurePage() {
   const activeCharacter = CharacterComponent();
   return (
